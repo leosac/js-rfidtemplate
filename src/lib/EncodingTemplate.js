6470ccdd5d3277e6cba840b8c72f0bd800408e5a
Maxhy <maxime.chamley@leosac.com>
2024-04-16 15:54:26 +0200

    Add JSON Schema

--- src/lib/EncodingTemplate.js
+++ src/lib/EncodingTemplate.js
@@ -1,9 +1,16 @@
 import React from "react";
+import Form from '@rjsf/bootstrap-4';
+import validator from '@rjsf/validator-ajv8';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./EncodingTemplate.css";
+
+import schema from '../json-schema.json';
+import uiSchema from "../ui-schema.json";
 
 class EncodingTemplate extends React.Component {
 	render() {
-		return (<div></div>);
+		return (<Form schema={schema} validator={validator} uiSchema={uiSchema} />);
 	}
 }
 
