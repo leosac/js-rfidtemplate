d3764ca6351b8b2592acfcee086f2330c77c00d9
Maxhy <maxime.chamley@leosac.com>
2024-05-16 14:55:41 +0200

    Add indicative arrow on Accordion cards

--- src/lib/ui-templates.js
+++ src/lib/ui-templates.js
@@ -31,34 +31,35 @@ function AccordionArrayFieldTemplate(props) {
                   )}
                 </div>
                 <div className="col-4 text-right">
-                {element.hasMoveUp && (
-                  <span className="m-0 p-0">
-                    <button title="Move up" style={{flex: '1 1 0%', paddingLeft: '6px', paddingRight: '6px', fontWeight: 'bold'}} disabled={element.index === 0} type="button" className="btn btn-light btn-sm" onClick={element.onReorderClick(element.index, element.index - 1)}>
-                      <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M868 545.5L536.1 163a31.96 31.96 0 0 0-48.3 0L156 545.5a7.97 7.97 0 0 0 6 13.2h81c4.6 0 9-2 12.1-5.5L474 300.9V864c0 4.4 3.6 8 8 8h60c4.4 0 8-3.6 8-8V300.9l218.9 252.3c3 3.5 7.4 5.5 12.1 5.5h81c6.8 0 10.5-8 6-13.2z"></path></svg>
-                    </button>
-                  </span>
-                )}
-                {element.hasMoveDown && (
-                  <span className="m-0 p-0">
-                    <button title="Move down" style={{flex: '1 1 0%', paddingLeft: '6px', paddingRight: '6px', fontWeight: 'bold'}} disabled={element.index === (props.items.length - 1)} type="button" className="btn btn-light btn-sm" onClick={element.onReorderClick(element.index, element.index + 1)}>
-                      <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M862 465.3h-81c-4.6 0-9 2-12.1 5.5L550 723.1V160c0-4.4-3.6-8-8-8h-60c-4.4 0-8 3.6-8 8v563.1L255.1 470.8c-3-3.5-7.4-5.5-12.1-5.5h-81c-6.8 0-10.5 8.1-6 13.2L487.9 861a31.96 31.96 0 0 0 48.3 0L868 478.5c4.5-5.2.8-13.2-6-13.2z"></path></svg>
-                    </button>
-                  </span>
-                )}
-                {element.hasCopy && (
-                  <span className="m-0 p-0">
-                    <button title="Copy" style={{flex: '1 1 0%', paddingLeft: '6px', paddingRight: '6px', fontWeight: 'bold'}} type="button" className="btn btn-light btn-sm" onClick={element.onCopyIndexClick(element.index)}>
-                      <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M352 115h90c3.3 0 6-2.7 6-6 0-8.2-3.7-16-10-21.3l-77.1-64.2c-4.9-4.1-14.2-7.4-20.6-7.4-4.1 0-7.4 3.3-7.4 7.4V96c.1 10.5 8.6 19 19.1 19z"></path><path d="M307 96V16H176c-17.6 0-32 14.4-32 32v336c0 17.6 14.4 32 32 32h240c17.6 0 32-14.4 32-32V141h-96c-24.8 0-45-20.2-45-45z"></path><path d="M116 412V80H96c-17.6 0-32 14.4-32 32v352c0 17.6 14.4 32 32 32h256c17.6 0 32-14.4 32-32v-20H148c-17.6 0-32-14.4-32-32z"></path></svg>
-                    </button>
-                  </span>
-                )}
-                {element.hasRemove && (
-                  <span className="m-0 p-0">
-                    <button title="Remove" style={{flex: '1 1 0%', paddingLeft: '6px', paddingRight: '6px', fontWeight: 'bold'}} type="button" className="btn btn-danger btn-sm" onClick={element.onDropIndexClick(element.index)}>
-                      <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M368.5 240h-225c-8.8 0-16 7.2-16 16 0 4.4 1.8 8.4 4.7 11.3 2.9 2.9 6.9 4.7 11.3 4.7h225c8.8 0 16-7.2 16-16s-7.2-16-16-16z"></path></svg>
-                    </button>
-                  </span>
-                )}
+                  {element.hasMoveUp && (
+                    <span className="m-0 p-0">
+                      <button title="Move up" style={{flex: '1 1 0%', paddingLeft: '6px', paddingRight: '6px', fontWeight: 'bold'}} disabled={element.index === 0} type="button" className="btn btn-light btn-sm" onClick={element.onReorderClick(element.index, element.index - 1)}>
+                        <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M868 545.5L536.1 163a31.96 31.96 0 0 0-48.3 0L156 545.5a7.97 7.97 0 0 0 6 13.2h81c4.6 0 9-2 12.1-5.5L474 300.9V864c0 4.4 3.6 8 8 8h60c4.4 0 8-3.6 8-8V300.9l218.9 252.3c3 3.5 7.4 5.5 12.1 5.5h81c6.8 0 10.5-8 6-13.2z"></path></svg>
+                      </button>
+                    </span>
+                  )}
+                  {element.hasMoveDown && (
+                    <span className="m-0 p-0">
+                      <button title="Move down" style={{flex: '1 1 0%', paddingLeft: '6px', paddingRight: '6px', fontWeight: 'bold'}} disabled={element.index === (props.items.length - 1)} type="button" className="btn btn-light btn-sm" onClick={element.onReorderClick(element.index, element.index + 1)}>
+                        <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M862 465.3h-81c-4.6 0-9 2-12.1 5.5L550 723.1V160c0-4.4-3.6-8-8-8h-60c-4.4 0-8 3.6-8 8v563.1L255.1 470.8c-3-3.5-7.4-5.5-12.1-5.5h-81c-6.8 0-10.5 8.1-6 13.2L487.9 861a31.96 31.96 0 0 0 48.3 0L868 478.5c4.5-5.2.8-13.2-6-13.2z"></path></svg>
+                      </button>
+                    </span>
+                  )}
+                  {element.hasCopy && (
+                    <span className="m-0 p-0">
+                      <button title="Copy" style={{flex: '1 1 0%', paddingLeft: '6px', paddingRight: '6px', fontWeight: 'bold'}} type="button" className="btn btn-light btn-sm" onClick={element.onCopyIndexClick(element.index)}>
+                        <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M352 115h90c3.3 0 6-2.7 6-6 0-8.2-3.7-16-10-21.3l-77.1-64.2c-4.9-4.1-14.2-7.4-20.6-7.4-4.1 0-7.4 3.3-7.4 7.4V96c.1 10.5 8.6 19 19.1 19z"></path><path d="M307 96V16H176c-17.6 0-32 14.4-32 32v336c0 17.6 14.4 32 32 32h240c17.6 0 32-14.4 32-32V141h-96c-24.8 0-45-20.2-45-45z"></path><path d="M116 412V80H96c-17.6 0-32 14.4-32 32v352c0 17.6 14.4 32 32 32h256c17.6 0 32-14.4 32-32v-20H148c-17.6 0-32-14.4-32-32z"></path></svg>
+                      </button>
+                    </span>
+                  )}
+                  {element.hasRemove && (
+                    <span className="m-0 p-0">
+                      <button title="Remove" style={{flex: '1 1 0%', paddingLeft: '6px', paddingRight: '6px', fontWeight: 'bold'}} type="button" className="btn btn-danger btn-sm" onClick={element.onDropIndexClick(element.index)}>
+                        <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M368.5 240h-225c-8.8 0-16 7.2-16 16 0 4.4 1.8 8.4 4.7 11.3 2.9 2.9 6.9 4.7 11.3 4.7h225c8.8 0 16-7.2 16-16s-7.2-16-16-16z"></path></svg>
+                      </button>
+                    </span>
+                  )}
+                  <span class="rotate-icon">&gt;</span>
                 </div>
               </div>
             </Accordion.Toggle>
@@ -133,7 +134,14 @@ function ObjectFieldTemplate(props) {
       <Accordion>
         <Card>
           <Accordion.Toggle as={Card.Header} eventKey="0">
-            <h5>{props.title}</h5>
+            <div className="row">
+              <div className="col-8">
+                <h5>{props.title}</h5>
+              </div>
+              <div className="col-4 text-right">
+                <span class="rotate-icon">&gt;</span>
+              </div>
+            </div>
           </Accordion.Toggle>
           <Accordion.Collapse eventKey="0">
             <Card.Body>
